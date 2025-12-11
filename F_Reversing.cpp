15227632e88971a4db0cde4ec22355ac0dbf348b
Janardhan-Majumder <janardhan.md03@gmail.com>
2025-12-11 19:51:24 +0600

    some plm practice

---
+++ F_Reversing.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i <= (n - 1) / 2; i++)
+    {
+        // cout << a[i] << " ";
+        // cout << a[(n - 1) - i] << " ";
+        swap(a[i], a[(n - 1) - i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
--- M_Capital_or_Small_or_Digit.cpp
+++ M_Capital_or_Small_or_Digit.cpp
@@ -5,12 +5,20 @@ int main()
 {
     char x;
     cin >> x;
-    if ( 48 <=  (int)x && 57 >=  (int)x  )
+    if (48 <= (int)x && 57 >= (int)x)
     {
-       cout << "IS DIGIT" << endl; 
-    } else {
-          cout << "ALPHA" << endl; 
+        cout << "IS DIGIT" << endl;
     }
-    
+    else
+    {
+        cout << "ALPHA" << endl;
+        if (65 <= (int)x && 90 >= (int)x)
+        {
+            cout << "IS CAPITAL";
+        } else if (97 <= (int)x && 122 >= (int)x) {
+            cout << "IS SMALL";
+        }
+    }
+
     return 0;
 }
