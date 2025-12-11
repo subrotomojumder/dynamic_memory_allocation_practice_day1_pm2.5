b145d60a99c7c01227ac11b9fd37d04a4dfc0e28
Janardhan-Majumder <janardhan.md03@gmail.com>
2025-12-11 22:43:18 +0600

    solved all problem with dynamic array

---
+++ practice_p1.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int *get_array(int n)
+{
+    int *a = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    return a;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int * arr = get_array(n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    
+    return 0;
+}
